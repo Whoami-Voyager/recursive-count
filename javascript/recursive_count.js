5da3cc93067e339d2554362fa791ba2158ee762c
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-06-19 16:36:40 -0600

    ez

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,11 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num < 10){
+    console.log(num)
+    ++num;
+    recursiveCount(num)
+  } else {
+    return
+  }
 }
 
 if (require.main === module) {
@@ -11,3 +17,6 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// adds 1 to the number variable
+// calls the function again until variable is at 10
